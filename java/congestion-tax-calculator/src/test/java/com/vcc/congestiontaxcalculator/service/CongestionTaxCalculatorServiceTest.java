02f782e9ec9efe36bd67e14caf8ce3f426ca5e43
KPRAKAS1 <kishor.prakash@volvocars.com>
2023-03-05 02:50:23 +0100

    Using "Parameterized Tests" to all the cases

--- java/congestion-tax-calculator/src/test/java/com/vcc/congestiontaxcalculator/service/CongestionTaxCalculatorServiceTest.java
+++ java/congestion-tax-calculator/src/test/java/com/vcc/congestiontaxcalculator/service/CongestionTaxCalculatorServiceTest.java
@@ -9,17 +9,20 @@ import com.vcc.congestiontaxcalculator.model.config.TollFreeCalendar;
 import com.vcc.congestiontaxcalculator.model.dto.CalculateTaxPayloadDto;
 import com.vcc.congestiontaxcalculator.model.error.CongestionCalculatorException;
 import com.vcc.congestiontaxcalculator.service.model.CongestionTaxCalculatorFixture;
+import com.vcc.congestiontaxcalculator.service.model.TestCase;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.File;
@@ -29,6 +32,7 @@ import java.util.List;
 import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 @Slf4j
 public class CongestionTaxCalculatorServiceTest {
 
@@ -39,6 +43,8 @@ public class CongestionTaxCalculatorServiceTest {
     CongestionTaxCalculatorServiceImpl congestionTaxCalculatorService;
 
     private static CongestionTaxCalculatorFixture taxFixtures;
+    private static Map<Integer, TollFreeCalendar> tollFreeCalenderByYear = new HashMap<>();
+    private static Map<Integer, Map<Integer, List<Integer>>> readDaysByYearMonth = new HashMap<>();
 
     @BeforeAll
     public static void init() throws IOException {
@@ -46,14 +52,8 @@ public class CongestionTaxCalculatorServiceTest {
         oMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         File file = new File(CongestionTaxCalculatorServiceTest.class.getClassLoader().getResource("get-tax-fixture-1.json").getFile());
         taxFixtures = oMapper.readValue(file, CongestionTaxCalculatorFixture.class);
-    }
 
-    @BeforeEach
-    public void setUp()  {
         CongestionConfig cc = taxFixtures.getCongestionConfig();
-
-        Map<Integer, TollFreeCalendar> tollFreeCalenderByYear = new HashMap<>();
-        Map<Integer, Map<Integer, List<Integer>>> readDaysByYearMonth = new HashMap<>();
 
         cc.getTollFreeCalenders().forEach(tc -> {
             tollFreeCalenderByYear.put(tc.getYear(), tc);
@@ -65,33 +65,39 @@ public class CongestionTaxCalculatorServiceTest {
 
             readDaysByYearMonth.put(tc.getYear(), redDaysByMonth);
         });
+    }
 
-        ReflectionTestUtils.setField(configService,  "congestionConfig", cc);
+    @BeforeEach
+    public void setUp()  {
+        ReflectionTestUtils.setField(configService,  "congestionConfig", taxFixtures.getCongestionConfig());
         ReflectionTestUtils.setField(configService,  "tollFreeCalenderByYear", tollFreeCalenderByYear);
         ReflectionTestUtils.setField(configService,  "readDaysByYearMonth", readDaysByYearMonth);
 
         Mockito.when(configService.isTollFreeVehicle(ArgumentMatchers.any(Vehicle.class))).thenCallRealMethod();
-        Mockito.when(configService.fetchCongestionChargesForCity(ArgumentMatchers.anyString())).thenReturn(cc.getTaxRules().get(0).getCongestionCharges());
+        Mockito.when(configService.fetchCongestionChargesForCity(ArgumentMatchers.anyString())).thenReturn(taxFixtures.getCongestionConfig().getTaxRules().get(0).getCongestionCharges());
         Mockito.when(configService.isRedDay(ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt())).thenCallRealMethod();
         Mockito.when(configService.isTollFreeMonth(ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt())).thenCallRealMethod();
+
     }
 
-    @Test
-    public void testGetTax() {
 
-        taxFixtures.getTestCases().forEach(tc -> {
-            double actualTax = 0;
-            try {
-                actualTax = congestionTaxCalculatorService.getTax(
-                        new CalculateTaxPayloadDto(
-                                new Vehicle(tc.getVehicleType()),
-                                tc.getDateList(),
-                                "GOTHENBURG"));
-            } catch (CongestionCalculatorException e) {
+    @ParameterizedTest
+    @MethodSource("fetchAllTestCase")
+    public void testGetTax(TestCase tc){
+        double actualTax = 0;
+        try {
+            actualTax = congestionTaxCalculatorService.getTax(
+                    new CalculateTaxPayloadDto(
+                            new Vehicle(tc.getVehicleType()),
+                            tc.getDateList(),
+                            "GOTHENBURG"));
+        } catch (CongestionCalculatorException e) {
+        }
+        Assertions.assertEquals(tc.getExpectedTax(), actualTax, tc.getDescription());
+        log.info("Tested:"+tc.getDescription());
+    }
 
-            }
-            Assertions.assertEquals(tc.getExpectedTax(), actualTax, tc.getDescription());
-            log.info("Tested:"+tc.getDescription());
-        });
+    private static List<TestCase> fetchAllTestCase(){
+        return taxFixtures.getTestCases();
     }
 }
